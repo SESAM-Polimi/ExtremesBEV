8407dd5837c08c5ecd937bce3a2ba80c7524eb98
deboraghezzi <debora2.ghezzi@mail.polimi.it>
2024-06-12 15:16:05 +0200

    ember

--- Database_building.py
+++ Database_building.py
@@ -1,8 +1,9 @@
 #%%
 from fiona.core.db_builder import DB_builder
 import json
+import pandas as pd
 
-user = 'LR'
+user = 'DG'
 
 with open('Paths.json', 'r') as file:
     paths = json.load(file)[user]
@@ -18,6 +19,16 @@ db = DB_builder(
     sut_format='txt',
     read_master_file=False,
 )
+
+# %%
+nowcasting_year = 2023
+
+db.sut.aggregate("Aggregations/1. Nowcasting electricity.xlsx",ignore_nan=True)
+
+#%%
+ee_mixes = pd.read_excel("Data/EE mixes Ember-Exiobase.xlsx" ,sheet_name=str(nowcasting_year),index_col=[0])
+
+
 
 
 # %%
